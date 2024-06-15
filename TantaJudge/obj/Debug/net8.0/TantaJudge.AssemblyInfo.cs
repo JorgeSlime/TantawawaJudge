c8c8f09dd628bd9e00753c7ba8da05a5e05d8eae
JorgeSlime <jt603691@gmail.com>
2024-06-14 20:36:48 -0400

    v1.11

--- TantaJudge/obj/Debug/net8.0/TantaJudge.AssemblyInfo.cs
+++ TantaJudge/obj/Debug/net8.0/TantaJudge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TantaJudge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6850128b06393399e8eaf6a7e84923f975cd6f9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TantaJudge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TantaJudge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
